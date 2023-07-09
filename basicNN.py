217bc1581c7bce54b5df7df2b17f681bd9daa056
Deep Lekhak <lekhakdeepak10@gmail.com>
2023-07-09 13:55:20 -0400

    neural net -XOR

---
+++ basicNN.py
@@ -0,0 +1,117 @@
+import numpy as np
+import random 
+
+#Base Layer
+class Layer:
+    def __init__(self):
+        self.input=None
+        self.output=None
+        
+
+    def forward(self,input):
+        pass
+    
+
+    def backward(self, output_gradiebnt,learning_rate):
+        pass
+#base activation
+class Activation(Layer):
+    def __init__(self, activation, activation_prime):
+        self.activation = activation
+        self.activation_prime = activation_prime
+
+    def forward(self, input):
+        self.input = input
+        return self.activation(self.input)
+
+    def backward(self, output_gradient, learning_rate):
+        return np.multiply(output_gradient, self.activation_prime(self.input))
+
+
+
+#Dense or Hidden layer
+class Dense(Layer):
+    def __init__(self, input_size, output_size):
+        self.weights = np.random.randn(output_size, input_size)
+        self.bias = np.random.randn(output_size, 1)
+
+    def forward(self, input):
+        self.input = input
+        return np.dot(self.weights, self.input) + self.bias
+
+    def backward(self, output_gradient, learning_rate):
+        weights_gradient = np.dot(output_gradient, self.input.T)
+        input_gradient = np.dot(self.weights.T, output_gradient)
+        self.weights -= learning_rate * weights_gradient
+        self.bias -= learning_rate * output_gradient
+        return input_gradient
+    
+#Activation Layer/Functions
+class Tanh(Activation):#1.Tanh activation function
+    def __init__(self):
+        def tanh(x):
+            return np.tanh(x)
+
+        def tanh_prime(x):
+            return 1 - np.tanh(x) ** 2
+
+        super().__init__(tanh, tanh_prime)
+
+
+class Sigmoid(Activation):#2.Sigmoid activation function
+
+    def __init__(self):
+        def sigmoid(x):
+            return 1 / (1 + np.exp(-x))
+
+        def sigmoid_prime(x):
+            s = sigmoid(x)
+            return s * (1 - s)
+
+        super().__init__(sigmoid, sigmoid_prime)
+
+class Softmax(Layer):#3.Softmax activation function
+
+    def forward(self, input):
+        tmp = np.exp(input)
+        self.output = tmp / np.sum(tmp)
+        return self.output
+    
+    def backward(self, output_gradient, learning_rate):
+        n = np.size(self.output)
+        return np.dot((np.identity(n) - self.output.T) * self.output, output_gradient)
+
+
+#Loss Function        
+    def mse(y_true, y_pred):
+        return np.mean(np.power(y_true - y_pred, 2))
+
+    def mse_prime(y_true, y_pred):
+        return 2 * (y_pred - y_true) / np.size(y_true)
+
+
+#Network 
+def predict(network, input):
+    output = input
+    for layer in network:
+        output = layer.forward(output)
+    return output
+
+def train(network, loss, loss_prime, x_train, y_train, epochs = 1000, learning_rate = 0.01, verbose = True):
+    for e in range(epochs):
+        error = 0
+        for x, y in zip(x_train, y_train):
+            # forward
+            output = predict(network, x)
+
+            # error
+            error += loss(y, output)
+
+            # backward
+            grad = loss_prime(y, output)
+            for layer in reversed(network):
+                grad = layer.backward(grad, learning_rate)
+
+        error /= len(x_train)
+        if verbose:
+            print(f"{e + 1}/{epochs}, error={error}")
