4d6d02fdc85f6113e691316f8c128da50fbf9533
Deep Lekhak <lekhakdeepak10@gmail.com>
2023-07-09 14:21:07 -0400

    fixed function import error

--- basicNN.py
+++ basicNN.py
@@ -83,10 +83,10 @@ class Softmax(Layer):#3.Softmax activation function
 
 
 #Loss Function        
-    def mse(y_true, y_pred):
+def mse(y_true, y_pred):
         return np.mean(np.power(y_true - y_pred, 2))
 
-    def mse_prime(y_true, y_pred):
+def mse_prime(y_true, y_pred):
         return 2 * (y_pred - y_true) / np.size(y_true)
 
 
