b62da9e3730425383a2afa245409610397e0224f
upendra <pantupendra97@gmail.com>
2023-07-19 20:04:37 +0545

    updat

--- NumRecognizer.py
+++ NumRecognizer.py
@@ -15,28 +15,52 @@ class Layer:
         self.input = np.random.randn(input_size)
         return self.input
     
-    def ActivationReLU(self, z):
-        return np.maximum(0, z)
-    
-    def ActivationSoftmax(self, Z):
+    def softmax(self, Z):
         A = np.exp(Z) / np.sum(np.exp(Z))
         return A
+    
+    def sigmoid(self , x):
+        return 1/(1+np.exp(-x))
+    
 
+def NodeCost(z2_softmax , expOutput):
+    error = expOutput - z2_softmax
+    return error**2
+def NodeCostDerivative(z2_softmax , expOutput):
+    return 2*(z2_softmax - expOutput)
 
+def sigmoidDerivattive(x):
+    dev = layer.sigmoid(x)
+    return x * (1 - x)
+
+def mse(x, y):
+    return np.mean(np.power(x- y, 2))
+    
+def derv(self):
+    pass  
+
+#Created instance of the class
 layer = Layer()
+inputs =  [ 0.68544165 ,
+          -0.40785542]
 
-input_neurons = layer.inputInitializer(4)
-w1, b1 = layer.initParams(4, 8)
-z1 = layer.forward(input_neurons)
-z1_relu = layer.ActivationReLU(z1)
+w1, b1 = layer.initParams(2, 3)
+z1 = layer.forward(inputs)
+z1_A = layer.sigmoid(z1)
 
-w2, b2 = layer.initParams(8, 4)
-y = layer.forward(z1_relu)
-y_softmax = layer.ActivationSoftmax(y)
+w2, b2 = layer.initParams(3, 2)
+z2 = layer.forward(z1_A)
+z2_softmax = layer.softmax(z2)
 
-print('-' * 10, "Neural Network Tester", '-' * 30)
-print("\nInput Layer:\n", input_neurons)
-print("\nHidden Layer One:\n", "\nweights:\n", w1, "\n\nbiases:\n", b1)
-print("\nReLU Output:\n", z1_relu)
-print("\nOutput Layer:\n", "\nweights:\n", w2, "\n\nbiases:\n", b2)
-print("\nSoftmax Output:\n", y_softmax)
+expOutput = [1,0]
+
+cost = mse(z2_softmax,expOutput)
+
+
+# print("Inputs:\n", inputs)
+# print("\nHidden Layer:-\n", "weights:\n", w1, "\nbiases:\n", b1)
+# print("\nSigmoid Output:\n",z1)
+# print("\nOutput Layer:\n", "weights:\n", w2, "\nbiases:\n", b2)
+print("\nSoftmax Output:\n",z2_softmax)
+print("\nExpected Output:", expOutput)
+print("\nCost is:", cost)
